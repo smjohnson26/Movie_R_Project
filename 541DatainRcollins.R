64ac4520fea401603f27c01eef970b498032335f
camills4 <66037056+camills4@users.noreply.github.com>
2020-11-12 17:03:14 -0600

    Update 541DatainRcollins.R

--- 541DatainRcollins.R
+++ 541DatainRcollins.R
@@ -1,24 +1,29 @@
 library(readr)
-<<<<<<< Updated upstream
-IMDb_movies <- read_csv("./IMDb movies.csv", budget = col_double(), usa_gross_income = col_double(), worlwide_gross_income = col_double(), metascore = col_double())
-USAMOVIES <- read_csv("USAMOVIES.csv")
-=======
-library("stringr")
 library(leaps)
->>>>>>> Stashed changes
+library("stringr")
+
+USAMOVIES <- read_csv("MSBA/Fall/ST 541/Final Project/Movie_R_Project/USAMOVIES.csv")
+
+
+names(USAMOVIES)<-str_replace_all(names(USAMOVIES), c("_" = "." , "," = "" ))
+USAMOVIES$budget <- substr(USAMOVIES$budget, start = 2, stop = 15)
+USAMOVIES$budget <- as.numeric(USAMOVIES$budget)
+USAMOVIES$usa.gross.income <- substr(USAMOVIES$usa.gross.income, start = 2, stop = 15)
+USAMOVIES$usa.gross.income <- as.numeric(USAMOVIES$usa.gross.income)
+USAMOVIES$worlwide.gross.income <- substr(USAMOVIES$worlwide.gross.income, start = 2, stop = 15)
+USAMOVIES$worlwide.gross.income <- as.numeric(USAMOVIES$worlwide.gross.income)
 
 str(USAMOVIES)
 summary(USAMOVIES)
 
-<<<<<<< Updated upstream
 #Does the budget have an effect on the profit margins, total gross, and user ratings of the movie?
 #How are ratings affected by movie genre, movie duration, gender, or age group?
 #Are production companies a significant variable when discussing the success of a movie?
 #What is the best way to predict the profit margin of a movie?
 #What is the comparison of international movies to movies made in America by market share and has that trend evolved over the years?
-=======
+
 attach(USAMOVIES)
-#detach(USAMOVIES)
+detach(USAMOVIES)
 
 
 
